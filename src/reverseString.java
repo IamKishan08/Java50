63bf57e2551f4801807b13c3c0538e97c2dfa2db
iamkishan08 <kishanoffcl08@gmail.com>
2024-08-15 08:53:28 +0530

    reverse a string

---
+++ src/reverseString.java
@@ -0,0 +1,17 @@
+public class reverseString {
+    public static void main(String[] args) {
+        String s = "Hello";
+
+        StringBuilder rev = new StringBuilder();
+
+        char[] c = s.toCharArray();
+
+        for(int i = c.length-1 ; i>=0 ; i--){
+            rev.append(c[i]);
+        }
+
+        System.out.println(rev);
+
+    }
+
+}
