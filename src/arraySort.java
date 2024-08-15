599c27a8884819b7e245591be031ceaeba18e37f
iamkishan08 <kishanoffcl08@gmail.com>
2024-08-15 19:54:16 +0530

    Array Sorting using Inbuilt Function

---
+++ src/arraySort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class arraySort {
+    public static void main(String[] args) {
+        int[] arr = {56,-1,37,4,-10};
+        // TIM Sort
+        // Array Sorting using Inbuilt Function
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
